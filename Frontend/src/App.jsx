04d5ecaac055106c1268f79005c4c88150697385
Jay Sikarwar <jay2005sikarwar@gmail.com>
2026-07-16 14:50:53 +0530

    update style file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { RouterProvider } from "react-router"
 import { router } from "./app.routes.jsx"
 import { AuthProvider } from "./features/auth/auth.context.jsx"
-import { InterviewProvider } from "./features/interview/interview.context.jsx"
+// import { InterviewProvider } from "./features/interview/interview.context.jsx"
+import { InterviewProvider } from "./features/interview/style/interview.context.jsx";
 
 function App() {
 
